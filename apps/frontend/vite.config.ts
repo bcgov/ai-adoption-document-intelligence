85f7a4222cfb32b4d34d61f46734ccb372d363e6
dbarkowsky <dylanbarkowsky@gmail.com>
2025-11-26 09:26:09 -0800

    update react and vite plugin

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Resolve needed to address plugin-react v5 fast refresh issue.
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
   server: {
     port: 3000,
     host: true,
