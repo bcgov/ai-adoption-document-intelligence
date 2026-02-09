575149b7c859c305df382fd3151a7c69ca8d9aa9
dbarkowsky <dylanbarkowsky@gmail.com>
2026-02-09 15:50:08 -0800

    fix bugs, test endpoints

--- apps/backend-services/src/azure/dto/classifier.dto.ts
+++ apps/backend-services/src/azure/dto/classifier.dto.ts
@@ -1,6 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsString } from "class-validator";
-import { ClassifierSource, ClassifierStatus } from "@/generated";
+
+export enum ClassifierStatus {
+  PRETRAINING = "PRETRAINING",
+  FAILED = "FAILED",
+  TRAINING = "TRAINING",
+  READY = "READY",
+}
+
+export enum ClassifierSource {
+  AZURE = "AZURE",
+}
 
 export class ClassifierCreationDto {
   @ApiProperty()
