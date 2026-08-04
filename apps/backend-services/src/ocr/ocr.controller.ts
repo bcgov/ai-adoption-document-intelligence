07c73178cddaa152393db514ce3e595c6314c076
dbarkowsky <dylanbarkowsky@gmail.com>
2026-08-04 10:09:33 -0700

    update tables controller

--- apps/backend-services/src/ocr/ocr.controller.ts
+++ apps/backend-services/src/ocr/ocr.controller.ts
@@ -28,10 +28,8 @@ import {
 import { Permission } from "@/auth/role-permissions";
 import { DocumentService } from "@/document/document.service";
 import { ReprocessDocumentResponseDto } from "@/document/dto/reprocess-document-response.dto";
-import { GroupRole } from "@/generated";
 import { TrainingService } from "@/training/training.service";
 import { OcrService } from "./ocr.service";
-import { Permission } from "@/auth/role-permissions";
 
 @ApiTags("OCR")
 @Controller("api")
